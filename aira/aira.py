c072995ac09bd126bdbc1f506d586312a3c85d6e
Maki <sunwood.ai.labs@gmail.com>
2024-06-02 18:51:58 +0900

    ✨ [feat] AIRAの機能をアップデート
    - リポジトリ概要生成機能を追加
    - README生成機能を追加
    - Gaiah、HarmonAI実行処理を関数化
    - 設定ファイルのマージ処理を関数化

--- aira/aira.py
+++ aira/aira.py
@@ -7,42 +7,98 @@ from gaiah.cli import load_config
 from harmon_ai.harmon_ai import HarmonAI
 import pprint
 
+from litellm import completion
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
 class Aira:
     def __init__(self, config_path=".aira/config.yml"):
         logger.debug(f"aira config_path : {config_path}")
-        self.config = load_config(config_path)
-        gaiah_config_path = self.config["aira"]["gaiah"]["config_path"]
-        logger.debug(f"gaiah_config_path : {gaiah_config_path}")
-        self.gaiah_config = load_config(gaiah_config_path)
+        self.aira_config = load_config(config_path)
+        self.gaiah_config = {"gaiah" : self.aira_config["aira"]["gaiah"]}
         
-        self.repo_manager = RepositoryManager(self.gaiah_config)
-        self.commit_manager = CommitManager(self.config)
+        self.repo_manager = RepositoryManager(self.gaiah_config, self.aira_config)
+        self.commit_manager = CommitManager(self.aira_config)
 
-    def run(self):
-        logger.info("Gaiah run : {}".format(self.config["aira"]["gaiah"]["run"]))
-        if self.config["aira"]["gaiah"]["run"]:
+    def make_repo(self):
+        logger.info("Gaiah run : {}".format(self.aira_config["aira"]["gaiah"]["run"]))
+        if self.aira_config["aira"]["gaiah"]["run"]:
             logger.info("Gaiah section starting...")
             
             if not self.repo_manager.is_initialized():
                 logger.info("Initializing repository...")
                 self.repo_manager.init()
-                self.gaiah_config["gaiah"] = self.merge_configs(self.gaiah_config["gaiah"], self.gaiah_config["init"])
+                self.gaiah_config["gaiah"] = self.merge_configs(self.gaiah_config["gaiah"], self.gaiah_config["gaiah"]["init"])
+                
+                gaiah = Gaiah(self.gaiah_config)
+                gaiah.run()  # Gaiahの実行
             else:
                 logger.info("Develop repository...")
-                self.gaiah_config["gaiah"] = self.merge_configs(self.gaiah_config["gaiah"], self.gaiah_config["dev"])
+                self.gaiah_config["gaiah"] = self.merge_configs(self.gaiah_config["gaiah"], self.gaiah_config["gaiah"]["dev"])
             
-            pprint.pprint(self.gaiah_config)
-            gaiah = Gaiah(self.gaiah_config)
-            gaiah.run()  # Gaiahの実行
 
-        logger.info("Harmon AI run : {}".format(self.config["aira"]["harmon_ai"]["run"]))
-        if self.config["aira"]["harmon_ai"]["run"]:
+
+        logger.info("Harmon AI run : {}".format(self.aira_config["aira"]["harmon_ai"]["run"]))
+        if self.aira_config["aira"]["harmon_ai"]["run"]:
             logger.info("HarmonAI section starting...")
-            config_path = self.config["aira"]["harmon_ai"]["config_path"]
-            logger.info(f"config_path is {config_path}")
-            harmon_ai = HarmonAI(config_path=config_path)
-            harmon_ai.run()  # HarmonAIの実行
+                       
+            output_dir = r"C:/Prj/AIRA-Sample/AIRA-Sample01/.harmon_ai"
+            # output_dir = self.aira_config.get("repository_summary_output_dir", ".aira")
+            
+            # リポジトリ概要を生成
+            instructions_prompt_path = self.aira_config["aira"]["harmon_ai"]["instructions_prompt"]
+            repository_summary = self.repo_manager.generate_repository_summary(
+                instructions_prompt_path
+            )
+            
+            # リポジトリ概要を保存
+            output_file="repository_summary_raw.md"
+            self.repo_manager.save_repository_summary(repository_summary, output_dir, output_file)
+            
+            repository_summary_raw = os.path.join(output_dir, output_file)
+            with open(repository_summary_raw, "r", encoding="utf-8") as f:
+                repository_summary =  f.read()
 
+            # # README を生成
+            readme_prompt_template_path = r".aira\prompt_readme.md"
+            readme = self.generate_readme(
+                repository_summary, readme_prompt_template_path
+            )
+            
+            
+            output_file = "sections_template_llm.md"
+            self.repo_manager.save_repository_summary(readme, output_dir, output_file)
+            
+            harmon_ai_config = {"harmon_ai" : self.aira_config["aira"]["harmon_ai"]}
+            harmon_ai_config["harmon_ai"]["product"]["sections_content_file"] = output_file
+            harmon_ai = HarmonAI(config=harmon_ai_config)
+            harmon_ai.run()  # HarmonAIの実行
+    
+    def run(self):
+        logger.info("Gaiah run : {}".format(self.aira_config["aira"]["gaiah"]["run"]))
+        if self.aira_config["aira"]["gaiah"]["run"]:
+            logger.info("Gaiah section starting...")
+            
+            if self.repo_manager.is_initialized():
+                logger.info("Develop repository...")
+                self.gaiah_config["gaiah"] = self.merge_configs(self.gaiah_config["gaiah"], self.gaiah_config["gaiah"]["dev"])
+                gaiah = Gaiah(self.gaiah_config)
+                gaiah.run()  # Gaiahの実行
+
+    def co_and_merge_branches(self):
+        if self.aira_config["aira"]["gaiah"]["run"]:
+            logger.info("Gaiah section starting...")
+            
+            if self.repo_manager.is_initialized():
+                logger.info("Develop repository...")
+                self.gaiah_config["gaiah"] = self.merge_configs(self.gaiah_config["gaiah"], self.gaiah_config["gaiah"]["dev"])
+                gaiah = Gaiah(self.gaiah_config)
+                gaiah.checkout_and_merge_branches()  # Gaiahの実行
+                gaiah.push()  # Gaiahの実行
+    
     def merge_configs(self, base_config, override_config):
         merged_config = base_config.copy()
         for key, value in override_config.items():
@@ -51,3 +107,35 @@ class Aira:
             else:
                 merged_config[key] = value
         return merged_config
+    
+    
+    def generate_readme(self, repository_summary, prompt_template_path=None):
+        """
+        リポジトリの概要とテンプレートからREADMEを生成する
+        """
+        try:
+            sections_content_path = r".harmon_ai\sections_template.md"
+            with open(sections_content_path, "r", encoding="utf-8") as f:
+                template = f.read()
+
+            # プロンプトテンプレートファイルの読み込み
+            if prompt_template_path:
+                try:
+                    with open(prompt_template_path, "r", encoding="utf-8") as f:
+                        prompt_template = f.read()
+                except FileNotFoundError:
+                    logger.warning(f"Prompt template file not found: {prompt_template_path}. Using default template.")
+                    prompt_template = "下記のテンプレートに従ってリポジトリの概要からREADMEを作成して\n\n# テンプレート\n\n[テンプレート]\n\n# 概要\n\n[概要]"
+            else:
+                prompt_template = "下記のテンプレートに従ってリポジトリの概要からREADMEを作成して\n\n# テンプレート\n\n[テンプレート]\n\n# 概要\n\n[概要]"
+
+            prompt = prompt_template.replace("[readme_template]", template).replace("[readme_abst]", repository_summary)
+
+            response = completion(
+                model=self.aira_config["aira"]["llm"]["model"],
+                messages=[{"role": "user", "content": prompt}]
+            )
+            return response["choices"][0]["message"]["content"]
+        except Exception as e:
+            logger.error(f"Error generating README: {e}")
+            return None
