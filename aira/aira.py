106c8aa09f4dc1ff955e26cbcd091dd480d4be11
Maki <sunwood.ai.labs@gmail.com>
2024-06-02 15:43:11 +0900

    ✨ [feat] Airaの構造更新とGaiah設定ファイル分割への対応
    - GaiahRepoとGaiahCommitをRepositoryManagerとCommitManagerに置き換えました。
    - Gaiah設定ファイルの分割に対応し、初期化/開発時の設定を動的に読み込むように変更しました。
    - HarmonAIの実行機能を追加しました。
    - 設定ファイルのマージ機能を追加しました。

--- aira/aira.py
+++ aira/aira.py
@@ -1,10 +1,53 @@
+# aira/aira.py
 from loguru import logger
-from .gaiah_repo import GaiahRepo
-from .gaiah_commit import GaiahCommit
+from .repository_manager import RepositoryManager
+from .commit_manager import CommitManager
+from gaiah.gaiah import Gaiah
+from gaiah.cli import load_config
+from harmon_ai.harmon_ai import HarmonAI
+import pprint
 
 class Aira:
-    def __init__(self, args):
-        pass
+    def __init__(self, config_path=".aira/config.yml"):
+        logger.debug(f"aira config_path : {config_path}")
+        self.config = load_config(config_path)
+        gaiah_config_path = self.config["aira"]["gaiah"]["config_path"]
+        logger.debug(f"gaiah_config_path : {gaiah_config_path}")
+        self.gaiah_config = load_config(gaiah_config_path)
+        
+        self.repo_manager = RepositoryManager(self.gaiah_config)
+        self.commit_manager = CommitManager(self.config)
 
     def run(self):
-        pass
+        logger.info("Gaiah run : {}".format(self.config["aira"]["gaiah"]["run"]))
+        if self.config["aira"]["gaiah"]["run"]:
+            logger.info("Gaiah section starting...")
+            
+            if not self.repo_manager.is_initialized():
+                logger.info("Initializing repository...")
+                self.repo_manager.init()
+                self.gaiah_config["gaiah"] = self.merge_configs(self.gaiah_config["gaiah"], self.gaiah_config["init"])
+            else:
+                logger.info("Develop repository...")
+                self.gaiah_config["gaiah"] = self.merge_configs(self.gaiah_config["gaiah"], self.gaiah_config["dev"])
+            
+            pprint.pprint(self.gaiah_config)
+            gaiah = Gaiah(self.gaiah_config)
+            gaiah.run()  # Gaiahの実行
+
+        logger.info("Harmon AI run : {}".format(self.config["aira"]["harmon_ai"]["run"]))
+        if self.config["aira"]["harmon_ai"]["run"]:
+            logger.info("HarmonAI section starting...")
+            config_path = self.config["aira"]["harmon_ai"]["config_path"]
+            logger.info(f"config_path is {config_path}")
+            harmon_ai = HarmonAI(config_path=config_path)
+            harmon_ai.run()  # HarmonAIの実行
+
+    def merge_configs(self, base_config, override_config):
+        merged_config = base_config.copy()
+        for key, value in override_config.items():
+            if isinstance(value, dict):
+                merged_config[key] = self.merge_configs(merged_config.get(key, {}), value)
+            else:
+                merged_config[key] = value
+        return merged_config
